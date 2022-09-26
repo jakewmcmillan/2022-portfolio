104e0e48ef8b49618fd9a6914eed41faf9f84e4e
Jake McMillan <jakewmcmillan@gmail.com>
2022-09-26 17:43:53 -0400

    Updating portfolio container

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import NavTabs from './pages/NavTabs';
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+
+export default function PortfolioContainer() {
+    const [currentPage, setCurrentPage] = useState('Home');
+    const renderPage = () => {
+        if (currentPage === 'Home') {
+            return <Home />;
+        }
+        if (currentPage === 'Portfolio') {
+            return <Portfolio />;
+        }
+        return <Contact />;
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div>
+            <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+            {renderPage()}
+        </div>
+    );
+}
