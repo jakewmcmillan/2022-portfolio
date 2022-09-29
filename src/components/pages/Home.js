2cf075a94f5156296c2311e26b1df12c3ec24b68
Jake McMillan <jakewmcmillan@gmail.com>
2022-09-29 17:11:41 -0400

    Adding profile picture

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
+import profilePicture from '../assets/profile-picture.jpg';
+
 export default function Home() {
     return (
         <div class="m-5">
-            <h1>Home Page</h1>
+            <h1>Welcome!</h1>
             <p>
-                Welcome to my page!
+                I am a frontend Web developer with a background in Economics and Media.  I earned a certificate in Full Stack Web Development from the University of Pennsylvania’s Coding Boot Camp.  I earned a bachelor’s degree in Applied Economics with a minor in Media & Communications from Ursinus College.  My strengths include working in a team setting, communication, and creativity.
+                While at Ursinus College I was a member of the Lacrosse and Rugby teams.  I am a huge sports fan and love the Sixers, Eagles, and Ohio State Football.  My other interests include fantasy football, gaming, hip-hop, Magic: The Gathering, and collecting sneakers.
             </p>
+            <img className="profile-picture rounded-circle img-fluid" src={profilePicture} alt="Me"></img>
         </div>
     );
 }
